836170e2aa2747a0f3a57150057a681424491870
onchin <ochin@unomaha.edu>
2018-10-29 14:13:17 -0500

    small stuff

--- shopcart/admin.py
+++ shopcart/admin.py
@@ -11,41 +11,51 @@ from .models import ApplianceStyle
 from .models import Order
 from .models import OrderAppliances
 
+
 class CategoryList(admin.ModelAdmin):
     list_display = ('name',)
     ordering = ['name']
 
+
 class StyleList(admin.ModelAdmin):
-    list_display = ('name','category',)
+    list_display = ('name', 'category',)
     ordering = ['category']
 
+
 class FeatureList(admin.ModelAdmin):
-    list_display = ('name','category',)
+    list_display = ('name', 'category',)
     ordering = ['category']
+
 
 class BrandList(admin.ModelAdmin):
     list_display = ('name',)
     ordering = ['name']
 
+
 class ApplianceList(admin.ModelAdmin):
     list_display = ('name', 'model', 'price', 'category', 'brand')
     ordering = ['name']
 
+
 class ApplianceFeatureList(admin.ModelAdmin):
-    list_display = ('appliance','feature',)
+    list_display = ('appliance', 'feature',)
     ordering = ['appliance']
 
+
 class ApplianceStyleList(admin.ModelAdmin):
-    list_display = ('appliance','style',)
+    list_display = ('appliance', 'style',)
     ordering = ['appliance']
+
 
 class OrderList(admin.ModelAdmin):
     list_display = ('customer',)
     ordering = ['customer']
 
+
 class OrderAppliancesList(admin.ModelAdmin):
-    list_display = ('order','appliance', 'style',)
+    list_display = ('order', 'appliance', 'style',)
     ordering = ['order']
+
 
 admin.site.register(Category, CategoryList)
 admin.site.register(Style, StyleList)
--- shopcart/models.py
+++ shopcart/models.py
@@ -2,11 +2,13 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+
 class Category(models.Model):
     name = models.CharField(max_length=30)
 
     def __str__(self):
         return self.name
+
 
 class Style(models.Model):
     name = models.CharField(max_length=30)
@@ -15,6 +17,7 @@ class Style(models.Model):
     def __str__(self):
         return self.name + ' - (' + self.category + ')'
 
+
 class Feature(models.Model):
     name = models.CharField(max_length=30)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
@@ -22,32 +25,38 @@ class Feature(models.Model):
     def __str__(self):
         return self.name + ' - (' + self.category + ')'
 
+
 class Brand(models.Model):
     name = models.CharField(max_length=30)
 
     def __str__(self):
         return self.name
 
+
 class Appliance(models.Model):
     name = models.CharField(max_length=30)
     model = models.CharField(max_length=30)
-    price = models.DecimalField(max_digits=10,decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
 
+
 class AppliaceFeature(models.Model):
     appliance = models.ForeignKey(Appliance, on_delete=models.CASCADE)
     feature = models.ForeignKey(Feature, on_delete=models.CASCADE)
+
 
 class ApplianceStyle(models.Model):
     appliance = models.ForeignKey(Appliance, on_delete=models.CASCADE)
     style = models.ForeignKey(Style, on_delete=models.CASCADE)
 
+
 class Order(models.Model):
     customer = models.ForeignKey(User, on_delete=models.CASCADE)
+
 
 class OrderAppliances(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
